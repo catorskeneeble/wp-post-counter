296f47a0714c3b90aa230e7d63ef21381baa9569
catorskeneeble <catorskeneeble@gmail.com>
2015-08-18 21:19:19 -0400

    Unjanked a lot of stuff.

--- monthly-post-count.php
+++ monthly-post-count.php
@@ -1,35 +1,33 @@
-<?php
-
-/*
-Plugin Name: Monthly Post Reporter
-PluginURI:   http://koreanhln.com
-Description: This plugin provides an administrator
-             with a user interface for counting posts
-             by month.
-Version:     1.0
-Author:      Pak
-*/
-
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-add_action( 'admin_menu', 'monthly_post_count_admin_menu');
-
-function monthly_post_count_admin_menu() {
-	add_menu_page(
-		'Monthly Post Count', // 1
-		'Monthly Post Count', // 2
-		'delete_others_posts', // 3
-		'mpc-page.php', // 4
-		'mpc_page' // 5
-	);
-}
-
-function mpc_page()
-{
-	wp_enqueue_style('derp', plugin_dir_url(__FILE__) .'style.css');
-	wp_enqueue_style('jquery-ui-css', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.css');
-	wp_enqueue_script( 'jquery-ui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js' );
-	include 'mpc-page.php';
-}
-
-
+<?php
+
+/*
+
+Plugin Name: Post Counter
+Description: This plugin provides an administrator
+             with a user interface for counting posts
+             by a specified timeframe.
+Version:     1.0
+Author:      Pak
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+add_action( 'admin_menu', 'monthly_post_count_admin_menu');
+
+function monthly_post_count_admin_menu() {
+	add_menu_page(
+		'Monthly Post Count', // 1
+		'Monthly Post Count', // 2
+		'delete_others_posts', // 3
+		'mpc-page.php', // 4
+		'mpc_page' // 5
+	);
+}
+
+function mpc_page()
+{
+	wp_enqueue_style('derp', plugin_dir_url(__FILE__) .'style.css');
+	wp_enqueue_style('jquery-ui-css', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.css');
+	wp_enqueue_script( 'jquery-ui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js' );
+	include 'mpc-page.php';
+}
--- mpc-page.php
+++ mpc-page.php
@@ -1,64 +1,61 @@
-<?php
-	require_once "mpc-functions.php";
-?>
-
-<h1>Post Counter</h1>
-
-<form action="" method="post">
-	<div class="lol-q">
-		<?php wp_dropdown_users(); ?>
-	</div>
-
-	<div class="lol-q">
-		<select class="period" name="period">
-			<option value="mpc_get_posts_today">Today's Posts</option>
-			<option value="mpc_get_posts_yesterday">Yesterday's Posts</option>
-			<option value="mpc_get_posts_last_7_days">Posts From Last 7 Days</option>
-			<option value="mpc_get_posts_last_30_days">Posts From Last 30 Days</option>
-			<option value="mpc_get_posts_last_week">Posts From Last Week</option>
-			<option value="mpc_get_posts_current_month">Posts From Current Month</option>
-			<option value="mpc_get_posts_last_month">Posts From Last Month</option>
-			<option value="mpc_get_posts_custom_date_range">Posts From Custom Date Range</option>
-		</select>
-	</div>
-
-	<div class="lol-q startdate">
-		<label>Start Date</label>
-		<input id="startdate" name="startdate" class="datepicker" />
-	</div>
-	<div class="lol-q enddate">
-		<label>End Date</label>
-		<input id="enddate" name="enddate" class="datepicker" />
-	</div>
-
-	<div class="lol-q">
-		<input type="submit">
-	</div>
-</form>
-
-<?php
-
-if( isset($_POST['period']) && isset($_POST['user']) ) {
-	$user = $_POST['user'];
-	$period = $_POST['period'];
-
-	if($period == 'mpc_get_posts_custom_date_range' && isset($_POST['startdate']) && isset($_POST['enddate'])) {
-		$startdate = $_POST['startdate'];
-		$enddate = $_POST['enddate'];	
-
-		mpc_get_posts($period, $user, $startdate, $enddate);
-	} else {
-		mpc_get_posts($period, $user);
-	}
-
-	
-
-}
-
-?>
-
-<script>
-	jQuery(document).ready(function(){
-		jQuery('.datepicker').datepicker();	
-	});
+<?php
+	require_once "mpc-functions.php";
+?>
+
+<h1>Post Counter</h1>
+
+<form action="" method="post">
+	<div class="lol-q">
+		<?php wp_dropdown_users(); ?>
+	</div>
+
+	<div class="lol-q">
+		<select class="period" name="period">
+			<option value="mpc_get_posts_today">Today's Posts</option>
+			<option value="mpc_get_posts_yesterday">Yesterday's Posts</option>
+			<option value="mpc_get_posts_last_7_days">Posts From Last 7 Days</option>
+			<option value="mpc_get_posts_last_30_days">Posts From Last 30 Days</option>
+			<option value="mpc_get_posts_last_week">Posts From Last Week</option>
+			<option value="mpc_get_posts_current_month">Posts From Current Month</option>
+			<option value="mpc_get_posts_last_month">Posts From Last Month</option>
+			<option value="mpc_get_posts_custom_date_range">Posts From Custom Date Range</option>
+		</select>
+	</div>
+
+	<div class="lol-q startdate">
+		<label>Start Date</label>
+		<input id="startdate" name="startdate" class="datepicker" />
+	</div>
+	<div class="lol-q enddate">
+		<label>End Date</label>
+		<input id="enddate" name="enddate" class="datepicker" />
+	</div>
+
+	<div class="lol-q">
+		<input type="submit">
+	</div>
+</form>
+
+<?php
+
+if( isset($_POST['period']) && isset($_POST['user']) ) {
+	$user = $_POST['user'];
+	$period = $_POST['period'];
+
+	if($period == 'mpc_get_posts_custom_date_range' && isset($_POST['startdate']) && isset($_POST['enddate'])) {
+		$startdate = $_POST['startdate'];
+		$enddate = $_POST['enddate'];	
+
+		mpc_get_posts($period, $user, $startdate, $enddate);
+	} else {
+		mpc_get_posts($period, $user);
+	}
+}
+
+?>
+
+<script>
+	jQuery(document).ready(function(){
+		jQuery('.datepicker').datepicker();	
+	});
 </script>
